a0b7e120ee2b2aac7eaa99a9feaaebc33d5ef5be
Brad Fitzpatrick <brad@danga.com>
2010-05-16 16:11:36 -0700

    polish up server; add a LICENSE

--- server/garage-server.go
+++ server/garage-server.go
@@ -1,10 +1,30 @@
+// Copyright 2010 Brad Fitzpatrick <brad@danga.com>
+//
+// See LICENSE.
+
 package main
 
 import "crypto/hmac"
+import "exec"
 import "fmt"
 import "http"
+import "os"
 import "strconv"
 import "time"
+import "flag"
+
+var listen *string = flag.String("listen", "0.0.0.0:8081", "host:port to listen on")
+var x10Unit *string = flag.String("x10unit", "f9", "X10 unit to toggle.")
+var heyUPath *string = flag.String(
+	"heyupath", "/usr/local/bin/heyu", "Path to heyu binary")
+
+var sharedSecret string
+
+func GarageOpenError(conn *http.Conn, err os.Error) {
+	fmt.Println("Error opening garage: ", err)
+	conn.WriteHeader(http.StatusInternalServerError)
+	fmt.Fprintf(conn, "Error opening garage: %v", err)
+}
 
 func HandleGarage(conn *http.Conn, req *http.Request) {
 	timeString := req.FormValue("t")
@@ -28,8 +48,7 @@ func HandleGarage(conn *http.Conn, req *http.Request) {
 		return
 	}
 
-	secretHasher := hmac.NewSHA1([]byte("foo"))
-	secretHasher.Reset() // redundant
+	secretHasher := hmac.NewSHA1([]byte(sharedSecret))
 	fmt.Fprint(secretHasher, timeString)
 	expectedHash := fmt.Sprintf("%40x", secretHasher.Sum())
 
@@ -39,8 +58,40 @@ func HandleGarage(conn *http.Conn, req *http.Request) {
                 return
 	}
 
+	fmt.Println("Opening garage door...")
+
+	cmd, err := exec.Run(
+		*heyUPath,
+		[]string{"heyu", "on", *x10Unit},
+		os.Environ(),
+		"/",
+		exec.DevNull, // stdin
+		exec.DevNull, // stdout
+		exec.MergeWithStdout) // stderr
+	if err != nil {
+		GarageOpenError(conn, err)
+		return
+	}
+
+	fmt.Printf("Started heyu with pid %v\n", cmd.Pid)
+	waitmsg, err := cmd.Wait(0)
+	if err != nil {
+		GarageOpenError(conn, err)
+                return
+	}
+	fmt.Printf("WaitMsg: %v\n", waitmsg)
+
+	if waitmsg.WaitStatus != 0 {
+		conn.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(conn, "x10 command returned error opening garage: %v", err)
+		return
+	}
+
 	fmt.Fprint(conn, "Opened.")
-	fmt.Println("Opened garage door.")
+	fmt.Printf("Garage opened at %v from %v\n",
+		time.LocalTime(),
+		conn.RemoteAddr)
+
 }
 
 func HandleRoot(conn *http.Conn, req *http.Request) {
@@ -52,8 +103,24 @@ server.`);
 }
 
 func main() {
+	flag.Parse()
+
+	sharedSecret = os.Getenv("GARAGE_SECRET")
+	if len(sharedSecret) == 0 {
+		fmt.Fprintf(os.Stderr,
+			"No GARAGE_SECRET environment variable set.\n")
+		os.Exit(1)
+	}
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", HandleRoot)
 	mux.HandleFunc("/garage", HandleGarage)
-	http.ListenAndServe("0.0.0.0:8001", mux)
+
+	fmt.Printf("Starting to listen on http://%v/\n", *listen)
+	err := http.ListenAndServe(*listen, mux)
+	if err != nil {
+		fmt.Fprintf(os.Stderr,
+			"Error in http server: %v\n", err)
+		os.Exit(1)
+	}
 }
